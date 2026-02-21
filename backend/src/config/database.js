b7e1a306d30a5ff6f7b9fb7d5a3ef930f2577a1a
Rajudotin <balavenkatarajusingampalli@gmail.com>
2026-02-22 00:27:55 +0530

    check password

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,5 @@
 ﻿const { Sequelize } = require("sequelize");
-
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
