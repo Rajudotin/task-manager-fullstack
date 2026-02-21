a24d57b45460d3d85951fd1522e83b8c6b78645c
Rajudotin <balavenkatarajusingampalli@gmail.com>
2026-02-21 21:11:55 +0530

    fix http to https

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://task-manager-fullstack-production-83d5.up.railway.app/api/v1"
+  baseURL: "https://task-manager-fullstack-production-83d5.up.railway.app/api/v1"
 });
 
 API.interceptors.request.use((req) => {
