2d8028a180c50d391abfe73c7e385f045ae9688c
Rajudotin <balavenkatarajusingampalli@gmail.com>
2026-02-22 08:24:19 +0530

    change api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-fullstack-production-83d5.up.railway.app/api/v1"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((req) => {
